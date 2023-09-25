8c304e2664d4ab2104ba931d47f1d721b03c4471
alekslesik <alekslesik@gmail.com>
2023-09-25 15:30:46 +0300

    js to jquery

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -7,7 +7,28 @@ for (var i = 0; i < navLinks.length; i++) {
 	}
 }
 
-$('.input-file input[type=file]').on('change', function(){
+$('.input-file input[type=file]').on('change', function () {
 	let file = this.files[0];
 	$(this).next().html(file.name);
+});
+
+// Function to load file by URL
+function downloadFile(url) {
+	const link = $('<a>');
+	link.attr('href', url);
+	link.attr('download', url.split('/').pop()); // Получить имя файла из URL
+	$('body').append(link);
+	link[0].click();
+	link.remove();
+}
+
+// Handler when hover
+$(document).on('mouseover', function (e) {
+	if ($(e.target).hasClass('file')) {
+		const fileUrl = $(e.target).data('file-url');
+		$(e.target).css('cursor', 'pointer'); // Изменить курсор при наведении
+		$(e.target).on('click', function () {
+			downloadFile(fileUrl);
+		});
+	}
 });
